a486bed061a388dc57706273dc2c22c5e922b5bc
CDRomatron <shortymike123@hotmail.co.uk>
2023-10-25 22:02:56 +0100

    Create Client.py

---
+++ worlds/apeescape/Client.py
@@ -0,0 +1,45 @@
+from typing import TYPE_CHECKING, Optional, Dict, Set
+
+from NetUtils import ClientStatus
+import worlds._bizhawk as bizhawk
+from worlds._bizhawk.client import BizHawkClient
+
+if TYPE_CHECKING:
+    from worlds._bizhawk.context import BizHawkClientContext
+else:
+    BizHawkClientContext = object
+
+
+EXPECTED_ROM_NAME = "ape escape / AP 2"
+
+# These flags are communicated to the tracker as a bitfield using this order.
+# Modifying the order will cause undetectable autotracking issues.
+
+class ApeEscapeClient(BizHawkClient):
+    game = "Ape Escape"
+    system = "PSX"
+    local_checked_locations: Set[int]
+    local_set_events: Dict[str, bool]
+    local_found_key_items: Dict[str, bool]
+    goal_flag: int
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.local_checked_locations = set()
+        self.local_set_events = {}
+        self.local_found_key_items = {}
+
+    async def validate_rom(self, ctx: BizHawkClientContext) -> bool:
+        from CommonClient import logger
+        return True
+
+    async def set_auth(self, ctx: BizHawkClientContext) -> None:
+        x = 3
+
+    async def game_watcher(self, ctx: BizHawkClientContext) -> None:
+        try:
+            x = 3
+
+        except bizhawk.RequestFailedError:
+            # Exit handler and return to main loop to reconnect
+            pass
