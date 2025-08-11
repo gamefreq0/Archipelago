252a077c427009b22a056612ce1740e6d32f84da
gamefreq0 <gamefreq0@gmail.com>
2025-08-11 14:45:45 -0400

    Fix indentation

--- worlds/spyro/Client.py
+++ worlds/spyro/Client.py
@@ -7,46 +7,47 @@ import worlds._bizhawk as bizhawk
 from worlds._bizhawk.client import BizHawkClient
 
 if TYPE_CHECKING:
-  from worlds._bizhawk.context import BizHawkClientContext
+    from worlds._bizhawk.context import BizHawkClientContext
 
 logger = logging.getLogger("Client")
 
+
 class SpyroClient(BizHawkClient):
-  game: str = "Spyro the Dragon"
-  system: str = "PSX"
+    game = "Spyro the Dragon"
+    system = "PSX"
 
-  async def validate_rom(self, ctx: "BizHawkClientContext") -> bool:
-    spyro_id: bytes = struct.pack("<17s", b"BASCUS-94228SPYRO")
-    spyro_id_ram_address: int = 0xBA92
-    try:
-      # Check ROM name
-      # Hopefully this keeps the encoding right on big endian machines
-      read_bytes: bytes = ((await bizhawk.read(ctx.bizhawk_ctx, [(
-        spyro_id_ram_address, len(spyro_id),"MainRAM"
-      )])))[0]
-      if read_bytes != spyro_id:
-        # Do command processor cleanup here
-        return False
+    async def validate_rom(self, ctx: "BizHawkClientContext") -> bool:
+        spyro_id = struct.pack("<17s", b"BASCUS-94228SPYRO")
+        spyro_id_ram_address: int = 0xBA92
+        try:
+            # Check ROM name
+            # Hopefully this keeps the encoding right on big endian machines
+            read_bytes: bytes = ((await bizhawk.read(ctx.bizhawk_ctx, [(
+                spyro_id_ram_address, len(spyro_id), "MainRAM"
+            )])))[0]
+            if read_bytes != spyro_id:
+                # Do command processor cleanup here
+                return False
 
-    except bizhawk.RequestFailedError:
-      # Do command processor cleanup here
-      return False # Not able to get a response, say no for now
+        except bizhawk.RequestFailedError:
+            # Do command processor cleanup here
+            return False  # Not able to get a response, say no for now
 
-    if not self.game == "Spyro the Dragon":
-      # Do command processor cleanup here
-      return False
+        if self.game != "Spyro the Dragon":
+            # Do command processor cleanup here
+            return False
 
-    ctx.game = self.game
-    # We want to be able to receive items from ourselves, and others
-    # Also handle starting inventory for initial level(s)
-    ctx.items_handling = 0b111
-    # Slot data will hold a lot of useful data for shuffling levels and such
-    ctx.want_slot_data = True
-    # Setup command processor here
-    return True
+        ctx.game = self.game
+        # We want to be able to receive items from ourselves, and others
+        # Also handle starting inventory for initial level(s)
+        ctx.items_handling = 0b111
+        # Slot data will hold a lot of useful data for shuffling levels and etc
+        ctx.want_slot_data = True
+        # Setup command processor here
+        return True
 
-  async def game_watcher(self, ctx: "BizHawkClientContext") -> None:
-    pass
+    async def game_watcher(self, ctx: "BizHawkClientContext") -> None:
+        pass
 
-  def __init__(self) -> None:
-    super().__init__()
+    def __init__(self) -> None:
+        pass
