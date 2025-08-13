f0c04e9306c78897c1df502740f23b795f7adcf1
gamefreq0 <gamefreq0@gmail.com>
2025-08-12 23:27:53 -0400

    Add more to world skeleton

--- worlds/spyro/__init__.py
+++ worlds/spyro/__init__.py
@@ -1,8 +1,15 @@
+from typing import final
+
 from BaseClasses import Item, Location, MultiWorld, Tutorial, ItemClassification
+from worlds.spyro.Options import SpyroOptions
 from ..AutoWorld import World, WebWorld
 from .Client import SpyroClient
+from .Items import SpyroItem
+from .Locations import SpyroLocation
+from .Options import SpyroOptions
 
 
+@final
 class SpyroWorld(World):
     """
     Spyro the Dragon, originally release on the PS1 in 1998, is a 3D collectathon platform starring the titular
@@ -10,3 +17,7 @@ class SpyroWorld(World):
     homeworlds, as you collect gems, recover stolen dragon eggs, free crystallized dragons, and make your way
     to Gnasty's World to torch him and save the world of dragons.
     """
+    game = "Spyro the Dragon"
+    options_dataclass = SpyroOptions
+    options: SpyroOptions
+    topology_present = True
