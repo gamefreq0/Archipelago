e18365bc5ef955553c6ed1832b08abf8fad18b90
gamefreq0 <gamefreq0@gmail.com>
2025-08-13 11:48:13 -0400

    Fix world docstring

--- worlds/spyro/__init__.py
+++ worlds/spyro/__init__.py
@@ -11,10 +11,10 @@ from .Options import SpyroOptions
 @final
 class SpyroWorld(World):
     """
-    Spyro the Dragon, originally release on the PS1 in 1998, is a 3D collectathon platform starring the titular
-    purple dragon, Spyro. Charge, jump, flame, and glide your way through 28 different levels across 6 different
+    Spyro the Dragon, originally released on the PS1 in 1998, is a 3D collectathon platform starring the titular
+    purple dragon, Spyro. Charge, jump, flame, and glide your way through 29 different levels across 6 different
     homeworlds, as you collect gems, recover stolen dragon eggs, free crystallized dragons, and make your way
-    to Gnasty's World to torch him and save the world of dragons.
+    to Gnorc Gnexus to torch Gnasty and save the world of dragons.
     """
     game = "Spyro the Dragon"
     options_dataclass = SpyroOptions
@@ -23,4 +23,3 @@ class SpyroWorld(World):
 
     item_name_to_id = {v: k for k, v in item_table.items()}
     location_name_to_id = {v: k for k, v in location_table.items()}
-    
