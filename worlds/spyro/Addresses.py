3921dc4227827b614d45e49fcd3fe3e2fdf9bfc4
gamefreq0 <gamefreq0@gmail.com>
2025-08-17 15:35:39 -0400

    Add doc strings, replace dicts with enums

--- worlds/spyro/Addresses.py
+++ worlds/spyro/Addresses.py
@@ -1,41 +1,53 @@
+from enum import Enum
+
+
 class RAM:
+    """A handy collection of memory values and addresses for Spyro"""
+    unusedSpace: int = 0x0f000  # At least, it seems unused. Test...
+    lastReceivedArchipelagoID: int = unusedSpace + 0x4
+    """The ID of the last item the game has been given. Useful for save state issues and such."""
     curLevelID: int = 0x7596c
     destLevelID: int = 0x758b4
     curGameState: int = 0x757d8
     balloonistMenuChoice: int = 0x777f0
     unlockedWorlds: int = 0x758d0
     startingLevelID: int = 0x2d4f0
-    worldTextOffsets: dict[str, int] = {
-        "artisans": 0x1006c,
-        "keepers": 0x1005c,
-        "crafters": 0x1004c,
-        "makers": 0x1003c,
-        "weavers": 0x1002c,
-        "gnasty": 0x1001c
-    }
+    """Which level you start in after the intro cutscene."""
+
+    class WorldTextOffsets(Enum):
+        """RAM offsets for world names"""
+        ARTISANS = 0x1006c
+        KEEPERS = 0x1005c
+        CRAFTERS = 0x1004c
+        MAKERS = 0x1003c
+        WEAVERS = 0x1002c
+        GNASTY = 0x1001c
+
     nestorUnskippable: int = 0x1747f4
     spyroCurAnimation: int = 0x78ad0
     spyroColorFilter: int = 0x78a80
-    spyroStates: dict[str, int] = {
-        "standing": 0x00,
-        "flop": 0x06,
-        "roll": 0x13,
-        "death_spin": 0x1e
-    }
-    gameStates: dict[str, int] = {
-        "gameplay": 0x00,
-        "loading": 0x01,
-        "paused": 0x02,
-        "inventory": 0x03,
-        "death": 0x04,
-        "game_over": 0x05,
-        "flight_menu": 0x07,
-        "dragon_cutscene": 0x08,
-        "fly_in": 0x09,
-        "exiting_level": 0x0a,
-        "fairy_textbox": 0x0b,
-        "balloonist": 0x0c,
-        "title_screen": 0x0d,
-        "cutscene": 0x0e,
-        "credits": 0x0f
-    }
+
+    class SpyroStates(Enum):
+        """Animation states for Spyro"""
+        STANDING = 0x00
+        FLOP = 0x06
+        ROLL = 0x13
+        DEATH_SPIN = 0x1e
+
+    class GameStates(Enum):
+        """States the Spyro game engine can be in"""
+        GAMEPLAY = 0x00
+        LOADING = 0x01
+        PAUSED = 0x02
+        INVENTORY = 0x03
+        DEATH = 0x04
+        GAME_OVER = 0x05
+        FLIGHT_MENU = 0x07
+        DRAGON_CUTSCENE = 0x08
+        FLY_IN = 0x09
+        EXITING_LEVEL = 0x0a
+        FAIRY_TEXTBOX = 0x0b
+        BALLOONIST = 0x0c
+        TITLE_SCREEN = 0x0d
+        CUTSCENE = 0x0e
+        CREDITS = 0x0f
