7f1463780d6f2812542d8949521962583dfcd677
J. "Dorian Greyson" L. <greysondn@gmail.com>
2025-08-24 07:39:00 -0400

    readability improvements

--- worlds/spyro/rules.py
+++ worlds/spyro/rules.py
@@ -6,6 +6,5 @@ except ImportError:
 if TYPE_CHECKING:
     from .world import SpyroWorld
 
-
 def set_rules(world: "SpyroWorld"):
     world.multiworld.completion_condition[world.player] = lambda state: state.has("Victory", world.player, 1)
