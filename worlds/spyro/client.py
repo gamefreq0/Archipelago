9841159026c66d0b000b1540885f72f3e30a2411
J. "Dorian Greyson" L. <greysondn@gmail.com>
2025-08-24 07:15:52 -0400

    readability and doc improvements

--- worlds/spyro/client.py
+++ worlds/spyro/client.py
@@ -87,11 +87,13 @@ class SpyroClient(BizHawkClient):
             or (ctx.slot_data is None) or (ctx.auth is None)
         ):
             return
+        
         if ctx.watcher_timeout != 0.125:
             # Hopefully this runs the first time. Good place for init stuff, if so
             # Might break in the future, even if it does.
             # TODO: replace with own bool
             ctx.watcher_timeout = 0.125
+        
         for item in ctx.items_received:
             match item_id_to_name[item.item]:
                 case "Victory":
@@ -122,11 +124,13 @@ class SpyroClient(BizHawkClient):
                         for level in hub.child_environments:
                             if level.name == item_name:
                                 self.portal_accesses[level.name] = True
+        
         if self.slot_data_spyro_color is None:
             color_string = ctx.slot_data["spyro_color"]
             if color_string is not None:
                 color_value: int = int(color_string, 16)
                 self.slot_data_spyro_color = color_value.to_bytes(4, byteorder="big")
+
         try:
             to_read_list: list[tuple[int, int]] = [
                 (RAM.last_received_archipelago_id, 4),
@@ -137,7 +141,9 @@ class SpyroClient(BizHawkClient):
                 (RAM.unlocked_worlds, 6),
                 (RAM.balloonist_menu_choice, 1)
             ]
+            
             temp_translation_info: dict[int, int] = {}
+            
             for level_id in self.gem_counts:
                 temp_translation_info[level_id] = len(to_read_list)
                 for hub in RAM.hub_environments:
@@ -146,8 +152,10 @@ class SpyroClient(BizHawkClient):
                     for level in hub.child_environments:
                         if level.internal_id == level_id:
                             to_read_list.append((level.gem_counter, 2))
+            
             for address, size in to_read_list:
                 batched_reads.append((address, size, "MainRAM"))
+            
             ram_data = await bizhawk.read(ctx.bizhawk_ctx, batched_reads)
 
             recv_index = int.from_bytes(ram_data[0], byteorder="little")
@@ -157,8 +165,10 @@ class SpyroClient(BizHawkClient):
             gnasty_anim_flag = int.from_bytes(ram_data[4], byteorder="little")
             unlocked_worlds = ram_data[5]
             balloonist_choice = int.from_bytes(ram_data[6], byteorder="little")
+            
             for level_id, gem_count_index in temp_translation_info.items():
                 self.gem_counts[level_id] = int.from_bytes(ram_data[gem_count_index], byteorder="little")
+                
             if cur_game_state == RAM.GameStates.GAMEPLAY:
                 for hub in RAM.hub_environments:
                     for level in hub.child_environments:
@@ -168,8 +178,10 @@ class SpyroClient(BizHawkClient):
                             and (gnasty_anim_flag == RAM.GNASTY_DEFEATED)
                         ):
                             await self.send_location_once("Defeated Gnasty Gnorc", ctx)
+                            
             for hub in RAM.hub_environments:
                 hub_quarter_count: int = int(hub.total_gems / 4)
+                
                 if self.gem_counts[hub.internal_id] >= hub_quarter_count:
                     await self.send_location_once(f"{hub.name} 25% Gems", ctx)
                 if self.gem_counts[hub.internal_id] >= (hub_quarter_count * 2):
@@ -178,8 +190,10 @@ class SpyroClient(BizHawkClient):
                     await self.send_location_once(f"{hub.name} 75% Gems", ctx)
                 if self.gem_counts[hub.internal_id] >= hub.total_gems:
                     await self.send_location_once(f"{hub.name} 100% Gems", ctx)
+                    
                 for level in hub.child_environments:
                     quarter_count: int = int(level.total_gems / 4)
+                    
                     if self.gem_counts[level.internal_id] >= quarter_count:
                         await self.send_location_once(f"{level.name} 25% Gems", ctx)
                     if self.gem_counts[level.internal_id] >= (quarter_count * 2):
@@ -200,11 +214,13 @@ class SpyroClient(BizHawkClient):
             ):
                 spyro_color = int.from_bytes(self.slot_data_spyro_color, "little")
                 to_write_ingame.append((RAM.spyro_color_filter, spyro_color.to_bytes(4, "little")))
+            
             if (
                 (cur_game_state == RAM.GameStates.GAMEPLAY)
                 and (unlocked_worlds.count(bytes([0])) > 1)
             ):
                 to_write_ingame.append((RAM.unlocked_worlds, bytes([2, 2, 2, 2, 2, 2])))
+            
             if cur_game_state == RAM.GameStates.GAMEPLAY:
                 # Overwrite head checking code
                 for hub in RAM.hub_environments:
@@ -216,6 +232,7 @@ class SpyroClient(BizHawkClient):
                             # NOP out the conditional branches
                             # This forces the statue heads to always open
                             to_write_ingame.append((address, bytes(4)))
+            
             # Lock inaccessible portals
             if cur_game_state == RAM.GameStates.GAMEPLAY:
                 for hub in RAM.hub_environments:
@@ -227,17 +244,18 @@ class SpyroClient(BizHawkClient):
                                 to_write_ingame.append((hub.portal_surface_types[index], b'\x00'))
 
             if cur_game_state == RAM.GameStates.TITLE_SCREEN:
-
                 starting_world_value = ctx.slot_data["starting_world"]
                 if starting_world_value is not None:
                     starting_world_value += 1
                     starting_world_value *= 10
                     to_write_menu.append((RAM.starting_level_id, starting_world_value.to_bytes(1, "little")))
+                    
             if (
                 (cur_game_state == RAM.GameStates.GAMEPLAY)
                 and (cur_level_id == 10)
             ):
                 to_write_ingame.append((RAM.nestor_unskippable, 0x0.to_bytes(1, "little")))
+                
             if cur_game_state == RAM.GameStates.BALLOONIST:
                 # Hide world names if inaccessible
                 for index, hub in enumerate(RAM.hub_environments):
@@ -250,6 +268,7 @@ class SpyroClient(BizHawkClient):
                         if not self.boss_items.count(True) == 5:
                             byte_val = b'\x00'
                         to_write_balloonist.append((hub.text_offset, byte_val))
+                
                 # Prevent access to inaccessible worlds
                 for index, hub in enumerate(RAM.hub_environments):
                     if cur_level_id == hub.internal_id:
@@ -274,11 +293,13 @@ class SpyroClient(BizHawkClient):
                 RAM.GameStates.GAMEPLAY.to_bytes(1, "little"),
                 ctx
             )
+            
             await self.write_on_state(
                 to_write_menu,
                 RAM.GameStates.TITLE_SCREEN.to_bytes(1, "little"),
                 ctx
             )
+            
             await self.write_on_state(
                 to_write_balloonist,
                 RAM.GameStates.BALLOONIST.to_bytes(1, "little"),
@@ -323,10 +344,13 @@ class SpyroClient(BizHawkClient):
         Args:
             write_list: entries in the form of (address, bytes to write)
             state: game state
+            ctx: BizhawkClientContext
         """
         to_write_list: list[tuple[int, bytes, str]] = []
+        
         for item in write_list:
             to_write_list.append((item[0], item[1], "MainRAM"))
+            
         if len(write_list) > 0:
             _ = await bizhawk.guarded_write(
                 ctx.bizhawk_ctx, to_write_list, [(RAM.cur_game_state, state, "MainRAM")]
@@ -340,6 +364,7 @@ class SpyroClient(BizHawkClient):
             location_name: The name of the location to send
         """
         location_id = location_name_to_id[location_name]
+        
         if location_id not in ctx.checked_locations:
             await ctx.send_msgs([{"cmd": "LocationChecks", "locations": [location_id]}])
 
