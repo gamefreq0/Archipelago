9082d35720a7d640915cd139ceda5fd4cc95a6f3
gamefreq0 <gamefreq0@gmail.com>
2025-08-31 13:44:08 -0400

    Fix type annotations

--- worlds/spyro/options.py
+++ worlds/spyro/options.py
@@ -1,18 +1,8 @@
 from dataclasses import dataclass
 
-from typing import TYPE_CHECKING
-try:
-    from typing import final
-except ImportError:
-    if TYPE_CHECKING:
-        from typing import final
-    else:
-        from typing_extensions import final
-
 from Options import Choice, DeathLink, PerGameCommonOptions, FreeText, Toggle
 
 
-@final
 class GoalOption(Choice):
     """Choose the victory condition for this world.
 
@@ -20,13 +10,12 @@ class GoalOption(Choice):
     loot: Reach the vortex at the end of Gnasty's Loot
     """
 
-    display_name = "Goal"
-    option_gnasty = 0x00
-    option_loot = 0x01
-    default = option_gnasty
+    display_name: str = "Goal"
+    option_gnasty: int = 0x00
+    option_loot: int = 0x01
+    default: int = option_gnasty
 
 
-@final
 class StartingHomeworldOption(Choice):
     """Choose which homeworld to start in.
 
@@ -38,36 +27,33 @@ class StartingHomeworldOption(Choice):
     dream_weavers
     """
 
-    display_name = "Starting Homeworld"
-    option_artisans = 0x00
-    option_peace_keepers = 0x01
-    option_magic_crafters = 0x02
-    option_beast_makers = 0x03
-    option_dream_weavers = 0x04
-    default = option_artisans
+    display_name: str = "Starting Homeworld"
+    option_artisans: int = 0x00
+    option_peace_keepers: int = 0x01
+    option_magic_crafters: int = 0x02
+    option_beast_makers: int = 0x03
+    option_dream_weavers: int = 0x04
+    default: int = option_artisans
 
 
-@final
 class PortalShuffleOption(Toggle):
     """If enabled, where a portal leads to is shuffled."""
 
-    display_name = "Portal Shuffle"
-    default = False
+    display_name: str = "Portal Shuffle"
+    default: bool = False
 
 
-@final
 class SpyroColorOption(FreeText):
     """Choose a color to tint Spyro, in RGBA format.
 
     Note that a value of FFFFFF00 will result in the vanilla colors.
     """
 
-    display_name = "Spyro Color"
-    default = "FFFFFF00"
+    display_name: str = "Spyro Color"
+    default: str = "FFFFFF00"
 
 
 @dataclass
-@final
 class SpyroOptions(PerGameCommonOptions):
     goal: GoalOption
     starting_world: StartingHomeworldOption
